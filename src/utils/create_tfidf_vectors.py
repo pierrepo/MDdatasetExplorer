1f13052d0e25553e9eeb628eb0da872e3325c407
uvsq22004047 <essmay.touami@ens.uvsq.fr>
2025-01-11 00:32:12 +0100

    Cleaning the text as embeddings creation.

--- src/utils/create_tfidf_vectors.py
+++ src/utils/create_tfidf_vectors.py
@@ -1,17 +1,24 @@
-"""Generate TF-IDF Vectors for Molecular Dynamics Datasets.
+"""Create TF-IDF Vectors for Molecular Dynamics Datasets
 
 TF-IDF (Term Frequency-Inverse Document Frequency) is a statistical measure used to evaluate the importance of a word in a document relative to a collection of documents.
 It helps to highlight relevant terms while reducing the weight of common words across all documents.
 
 Usage:
 ======
-    python src/utils/create_tfidf_vectors.py
+    python src/utils/create_tfidf_vectors.py --dataset_file_name <dataset_file_name>
 
-This command will process JSON files in the results/datasets directory, computes TF-IDF vectors for the text data,
-and stores the resulting vectors in the results/tfidf_vectors directory. Each dataset is processed individually, and its vectors are stored
-in a dedicated Chroma database under results/tfidf_vectors/chroma_db_tfidf_vectors_<dataset_name>.
+Argument:
+=========
+    --dataset_file_name : str
+        The file name of the JSON file with the datasets to process.
+
+Example:
+========
+    python src/utils/create_tfidf_vectors.py --dataset_file_name "extended_dataset.json"
+
+This command will generate TF-IDF vectors for all datasets in the `extended_dataset.json` file in the `results/datasets` directory.
+The resulting vectors will be stored in the `results/tfidf_vectors` directory in a chroma database named `chroma_db_tfidf_vectors_<dataset_name>`.
 """
-
 
 # METADATA
 __authors__ = ("Pierre Poulain", "Essmay Touami")
@@ -23,16 +30,15 @@ __version__ = "1.0.0"
 
 # LIBRARY IMPORTS
 import os
-import re
 import json
+import argparse
+import numpy as np
 from tqdm import tqdm
-from typing import List, Dict
-
-import nltk
-import chromadb
-from loguru import logger
-from sklearn.preprocessing import normalize
 from sklearn.feature_extraction.text import TfidfVectorizer
+from loguru import logger
+import chromadb
+from chromadb.utils import embedding_functions
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
 
 # CONSTANTS
@@ -41,177 +47,209 @@ OUT_DIR = "results/tfidf_vectors"
 
 
 # FUNCTIONS
-def load_json_files(directory: str) -> List[Dict[str, dict]]:
-    """Load all JSON files from the specified directory and return a list of dictionaries
-       containing file path and JSON content for each dataset.
-    
+def get_args() -> str:
+    """Parse command-line arguments."""
+    logger.info("Parsing command-line arguments...")
+    parser = argparse.ArgumentParser(
+        description="Generate TF-IDF vectors for datasets."
+    )
+    parser.add_argument(
+        "--dataset_file_name",
+        type=str,
+        required=True,
+        help="The name of the JSON file containing the datasets to process.",
+    )
+    args = parser.parse_args()
+    logger.debug(f"Dataset file name: {args.dataset_file_name}")
+    logger.success("Command-line arguments parsed successfully. \n")
+    return args.dataset_file_name
+
+
+def load_json_file(file_path: str) -> dict:
+    """
+    Load a JSON file and return its content.
+
     Parameters
     ----------
-    directory : str
-        The directory containing the JSON files.
+    file_path : str
+        The path to the JSON file to load.
 
     Returns
     -------
-    json_data : List[Dict[str, dict]]
-        A list of dictionaries where each dictionary contains the file path (`file_path`)
-        and the corresponding JSON content (`json_content`).
+    dict
+        The content of the JSON file.
     """
-    logger.info(f"Loading JSON files from {directory}...")
-    json_data = []
-    counter = 0
-    
-    for filename in os.listdir(directory):
-        # Only process JSON files
-        if filename.endswith(".json"):
-            filepath = os.path.join(directory, filename)
-            
-            with open(filepath, 'r', encoding='utf-8') as file:
-                try:
-                    json_content = json.load(file)
-                    dataset_count = len(json_content) if isinstance(json_content, list) else 1
-                    json_data.append({
-                        'file_path': filepath,
-                        'json_content': json_content
-                    })
-                    counter += 1
-                    logger.debug(f"{counter}: {filename} | Path: {filepath} | Number of datasets: {dataset_count}")
-                except json.JSONDecodeError as e:
-                    logger.warning(f"Error decoding {filename}: {e}")
-
-    logger.success(f"Loaded {len(json_data)} JSON files successfully. \n")
-    
-    return json_data
+    logger.info(f"Loading JSON file from {file_path}...")
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            json_content = json.load(file)
+            dataset_count = len(json_content) if isinstance(json_content, list) else 1
+            logger.debug(f"File: {file_path} | Number of datasets: {dataset_count}")
+            logger.success(f"Loaded JSON file successfully: {file_path} \n")
+            return json_content
+    except json.JSONDecodeError as e:
+        logger.error(f"Error decoding {file_path}: {e}")
+        raise ValueError(f"Failed to load JSON file at {file_path}") from e
 
 
-def generate_tfidf_vectors(documents: List[str], vectorizer: TfidfVectorizer) -> List[List[float]]:
-    """Generate TF-IDF vectors from a list of documents using the provided vectorizer.
+def clean_and_concatenate(data: dict, excluded_keys=None, words_to_remove=None) -> str:
+    """
+    Clean and concatenate values from a dictionary, excluding certain keys.
 
     Parameters
     ----------
-    documents : List[str]
-        A list of documents to process.
+    data : dict
+        The dictionary containing the data.
+    excluded_keys : list, optional
+        Keys to exclude from the concatenation.
+    words_to_remove : list, optional
+        Words or phrases to remove from the concatenated string.
+
+    Returns
+    -------
+    str
+        The cleaned and concatenated string.
+    """
+    if excluded_keys is None:
+        excluded_keys = [
+            "id",
+            "origin",
+            "author",
+            "date_creation",
+            "url",
+            "dt",
+            "nsteps",
+            "temperature",
+            "thermostat",
+            "barostat",
+        ]
+
+    if words_to_remove is None:
+        words_to_remove = ["None", "none", "Unknown", "n/a", "undefined"]
+
+    concatenated_text_parts = []
+
+    for key, value in data.items():
+        if key in excluded_keys:
+            continue
+
+        if key.startswith("has") and value is True:
+            concatenated_text_parts.append(key)
+        elif key == "atom_number":
+            concatenated_text_parts.append(f"{key}={value}")
+        elif isinstance(value, list):
+            concatenated_text_parts.append(" ".join(str(item) for item in value))
+        elif value:
+            concatenated_text_parts.append(str(value))
+
+    concatenated_text = " ".join(concatenated_text_parts)
+
+    for word in words_to_remove:
+        concatenated_text = concatenated_text.replace(word, "")
+
+    return concatenated_text.strip()
+
+
+def process_batch(batch: list, vectorizer: TfidfVectorizer) -> np.ndarray:
+    """
+    Generate TF-IDF vectors for a batch of text data.
+
+    Parameters
+    ----------
+    batch : list
+        A list of cleaned text strings.
     vectorizer : TfidfVectorizer
-        The pre-fitted TF-IDF vectorizer to transform the documents.
-    
+        The TF-IDF vectorizer.
+
     Returns
     -------
-    tfidf_vectors : List[List[float]]
-        A list of normalized TF-IDF vectors for each document.
+    np.ndarray
+        The TF-IDF vectors for the batch.
     """
-    # Transform the documents using the provided vectorizer
-    tfidf_matrix = vectorizer.transform(documents)
-    
-    # Normalize the TF-IDF vectors (to have a norm of 1)
-    normalized_tfidf = normalize(tfidf_matrix, norm='l2', axis=1)
-    
-    # Convert the sparse matrix to a dense array and return as list of lists
-    return normalized_tfidf.toarray()
+    return vectorizer.transform(batch).toarray()
 
 
-def store_in_chroma(vectors: List[List[float]], dataset_json: dict, dataset_name: str) -> None:
-    """Store TF-IDF vectors and corresponding metadata in a Chroma database.
-    
+def store_in_chroma(vectors: np.ndarray, dataset_json: dict, dataset_name: str) -> None:
+    """
+    Store TF-IDF vectors and corresponding metadata in a Chroma database.
+
     Parameters
     ----------
-    vectors : List[List[float]]
-        The TF-IDF vectors to store.
+    vectors : np.ndarray
+        A numpy array of TF-IDF vectors.
     dataset_json : dict
-        The metadata associated with the dataset.
+        The JSON content of the dataset.
     dataset_name : str
         The name of the dataset.
     """
-    logger.debug("Storing TF-IDF vectors in Chroma...")
-    # Initialize the Chroma client and database
-    chroma_db_path = os.path.join(OUT_DIR, f"chroma_db_tfidf_vectors_{dataset_name}")
+    if not os.path.exists(OUT_DIR):
+        os.makedirs(OUT_DIR)
+
+    chroma_db_path = os.path.join(OUT_DIR, f"chroma_db_{dataset_name}_tfidf")
     client = chromadb.PersistentClient(path=chroma_db_path)
-    # Create a new collection for the dataset
-    # Use cosine similarity for nearest neighbors
-    db = client.create_collection(name=f"chroma_db_tfidf_vectors_{dataset_name}", metadata={"hnsw:space": "cosine"})
+
+    db = client.create_collection(
+        name=f"chroma_db_{dataset_name}_tfidf",
+        metadata={"hnsw:space": "cosine"},
+    )
 
     for idx, vector in tqdm(
-        enumerate(vectors), 
-        desc=f"Storing TF-IDF vectors in Chroma", 
+        enumerate(vectors),
+        desc="Storing TF-IDF vectors in Chroma",
         total=len(vectors),
         unit="vector",
         ncols=100,
-        colour="blue"
+        colour="green",
     ):
         metadata = dataset_json[idx]
-        # Convert metadata values to strings
-        metadata_str = {key: str(value) if not isinstance(value, list) else ', '.join(str(v) for v in value) for key, value in metadata.items()}
-        # Use the ID from metadata as the unique identifier
         unique_id = metadata.get("id", idx)
-        # Store the vector and metadata in Chroma
+        metadata_str = {key: str(value) for key, value in metadata.items()}
         db.add(
             ids=[unique_id],
-            embeddings=vector,
+            embeddings=[vector.tolist()],
             metadatas=[metadata_str],
-            documents=[json.dumps(metadata)]
+            documents=[json.dumps(metadata)],
         )
+    logger.success(
+        f"Stored TF-IDF vectors for {dataset_name} in Chroma successfully.\n"
+    )
 
-    logger.success(f"Stored TF-IDF vectors for {dataset_name} in Chroma successfully.\n")
 
+def process_dataset(dataset: dict, vectorizer: TfidfVectorizer) -> None:
+    """
+    Process a single dataset and generate the corresponding TF-IDF vectors.
 
-def process_dataset(dataset: dict) -> None:
-    """Process a single dataset and generate the corresponding TF-IDF vectors.
-    
     Parameters
     ----------
     dataset : dict
-        The dataset to process. It contains the file path (`file_path`) and the JSON content (`json_content`).
+        The dataset to process.
+    vectorizer : TfidfVectorizer
+        The TF-IDF vectorizer.
     """
-    dataset_name = os.path.basename(dataset['file_path']).replace('.json', '')
+    dataset_name = os.path.basename(dataset["file_path"]).replace(".json", "")
     logger.info(f"Processing dataset: {dataset_name}")
 
-     # Create a specific vocabulary for each dataset
-    docs = [json.dumps(item) for item in dataset['json_content']]
-    combined_text = " ".join(docs)
-    unique_vocab = set(re.findall(r'\b\w+\b', combined_text))
-    logger.debug(f"There are {len(unique_vocab)} unique world.")
-    # Initialize and fit the TF-IDF vectorizer on the current dataset
-    vectorizer = TfidfVectorizer()
-    vectorizer.fit(unique_vocab)
+    documents = [clean_and_concatenate(item) for item in dataset["json_content"]]
 
-    # Extract documents and metadata from the dataset's JSON content
-    documents = [json.dumps(item) for item in dataset['json_content']]  # Convert each document to JSON string
-    
-    # Generate TF-IDF vectors (ensure they are normalized)
-    logger.debug("Generating TF-IDF vectors...")
-    vectors = []
+    vectors = process_batch(documents, vectorizer)
 
-    for document in tqdm(
-        documents, 
-        desc=f"Processing documents in {dataset_name}", 
-        unit="document", 
-        total=len(documents),
-        colour="blue",
-        ncols=100
-    ):
-        vectors.append(generate_tfidf_vectors([document], vectorizer)[0])
-    
-    # Store vectors and metadata in Chroma
-    store_in_chroma(vectors, dataset['json_content'], dataset_name)
+    store_in_chroma(vectors, dataset["json_content"], dataset_name)
 
 
 # MAIN PROGRAM
 if __name__ == "__main__":
-    # Create output directory if it does not exist
-    if not os.path.exists(OUT_DIR):
-        os.makedirs(OUT_DIR)
+    dataset_file_name = get_args()
 
-    # Necessary for tokenization
-    nltk.download('punkt')
+    logger.info("Initializing TF-IDF vectorizer...")
+    vectorizer = TfidfVectorizer(max_features=10000, stop_words="english")
 
-    # Load all datasets from the input directory
-    datasets = load_json_files(IN_DIR)
+    dataset_path = os.path.join(IN_DIR, dataset_file_name)
+    dataset_json = load_json_file(dataset_path)
 
-    # Combine all datasets to create a global vocabulary
-    all_documents = [json.dumps(item) for dataset in datasets for item in dataset['json_content']]
-    
-    # Process each dataset
-    for dataset in datasets:
-        # Process the dataset and store the TF-IDF vectors in Chroma
-        process_dataset(dataset)
+    logger.info("Fitting TF-IDF vectorizer...")
+    all_texts = [clean_and_concatenate(item) for item in dataset_json]
+    vectorizer.fit(all_texts)
 
-    logger.success(f" --- All datasets processed successfully. ---")
+    dataset = {"file_path": dataset_path, "json_content": dataset_json}
 
+    process_dataset(dataset, vectorizer)
